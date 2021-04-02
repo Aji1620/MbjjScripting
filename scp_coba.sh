178128fab5725358a835ea66a35eb4be9f2fbe9f
Aji1620 <81812877+Aji1620@users.noreply.github.com>
2021-04-02 20:22:24 +0700

    Update scp_coba.sh

--- scp_coba.sh
+++ scp_coba.sh
@@ -1,12 +1,13 @@
 tanggal=$1;
 
-thn=`echo ${tanggal} | cut -c1-4`;
-bln=`echo ${tanggal} | cut -c5-6`;
-tgl=`echo ${tanggal} | cut -c7-8`;
+if [ -z $tanggal ]
+then 
+   tanggal=`date +Y%m%d`
+fi
 
 for i in `sshpass -p 'Tselbi#123' ssh automationrpt@10.54.3.28 find /abusers/tsel/g_ops/L1/req/aji/*txt -type f -newermt ${thn}-${bln}-${tgl}`
 do
-   sshpass -p 'Tselbi#123' ssh automationrpt@10.54.3.28 'find $i -exec wc -c {} \;'
+   sshpass -p 'Tselbi#123' scp automationrpt@10.54.3.28 'find $i -exec wc -c {} \;'
 done
 
 echo "SCP Complete"
