4636e785e2c633f8aad51d31d8b2bb65285638b1
Mickel Edward <o2tongpethuk@gmail.com>
2021-04-02 20:26:31 +0700

    Add files via upload
    update date cut method

--- sorted_split_file.sh
+++ sorted_split_file.sh
@@ -1,18 +1,18 @@
 tanggal=$1
 
-thn=`echo ${tanggal} | cut -c1-4`
-bln=`echo ${tanggal} | cut -c5-6`
-tgl=`echo ${tanggal} | cut -c7-8`
+thn=${tanggal:0:4}
+bln=${tanggal:4:2}
+tgl=${tanggal:6:2}
 
 FILENAME="`find *.txt -type f -newermt ${thn}-${bln}-${tgl}`"
 
 for j in $FILENAME
 do
-        FILESIZE=`wc -l $j |awk '{print$1}'`
+        FILESIZE=$(wc -l $j |awk '{print$1}')
 
         if [ $FILESIZE -ge 20 ]
         then
-                xFILENAME="`echo $j |awk -F. '{print$1}'`"
+                xFILENAME="$(echo $j |awk -F. '{print$1}')"
                 sort -t ',' -k 2,2 -u $j > ${xFILENAME}_sorted.txt
                 split -l 10 -d --additional-suffix=.txt ${xFILENAME}_sorted.txt ${xFILENAME}_sorted_
                 echo "File ${xFILENAME}.txt Sudah selesai di split"
