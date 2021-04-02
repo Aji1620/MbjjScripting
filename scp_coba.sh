315f6f03badb1327bd5ad28d1c03d5faffde495c
Mickel Edward <o2tongpethuk@gmail.com>
2021-04-02 21:49:52 +0700

    small changes

--- scp_coba.sh
+++ scp_coba.sh
@@ -1,12 +1,14 @@
-tanggal=$1;
+tanggal=$1
+thn=${tanggal:0:4}
+bln=${tanggal:4:2}
+tgl=${tanggal:6:2}
+xtanggal="${thn}-${bln}-${tgl}"
 
-thn=`echo ${tanggal} | cut -c1-4`;
-bln=`echo ${tanggal} | cut -c5-6`;
-tgl=`echo ${tanggal} | cut -c7-8`;
+FILENAME="$(sshpass -p 'Tselbi#123' ssh automationrpt@10.54.3.28 find /abusers/tsel/g_ops/L1/req/aji/*txt -type f -newermt $xtanggal)"
 
-for i in `sshpass -p 'Tselbi#123' ssh automationrpt@10.54.3.28 find /abusers/tsel/g_ops/L1/req/aji/*txt -type f -newermt ${thn}-${bln}-${tgl}`
+for i in $FILENAME
 do
-   sshpass -p 'Tselbi#123' ssh automationrpt@10.54.3.28 'find $i -exec wc -c {} \;'
+	sshpass -p 'Tselbi#123' ssh automationrpt@10.54.3.28 'find $i -exec wc -c {} \;'
 done
 
 echo "SCP Complete"
