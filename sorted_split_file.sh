bec4c83d455c9f588821dbd0ba37b89a1868d9f7
Mickel Edward <o2tongpethuk@gmail.com>
2021-04-03 10:38:45 +0700

    small changes

--- sorted_split_file.sh
+++ sorted_split_file.sh
@@ -3,23 +3,23 @@ tanggal=$1
 thn=${tanggal:0:4}
 bln=${tanggal:4:2}
 tgl=${tanggal:6:2}
+xtanggal="${thn}-${bln}-${tgl}"
 
-FILENAME="`find *.txt -type f -newermt ${thn}-${bln}-${tgl}`"
+FILENAME="$(find *.txt -type f -newermt $xtanggal)"
 
 for j in $FILENAME
 do
-        FILESIZE=$(wc -l $j |awk '{print$1}')
-
-        if [ $FILESIZE -ge 20 ]
-        then
-                xFILENAME="$(echo $j |awk -F. '{print$1}')"
-                sort -t ',' -k 2,2 -u $j > ${xFILENAME}_sorted.txt
-                split -l 10 -d --additional-suffix=.txt ${xFILENAME}_sorted.txt ${xFILENAME}_sorted_
-                echo "File ${xFILENAME}.txt Sudah selesai di split"
-        elif [ $FILESIZE -lt 20 ]
-		then
-                xFILENAME="`echo $j |awk -F. '{print$1}'`"
-                sort -t ',' -k 2,2 -u $j > ${xFILENAME}_sorted.txt
-                echo "File ${xFILENAME}.txt Tidak perlu di split"
-        fi
+	FILESIZE=$(wc -l $j |awk '{print$1}')
+	if [ $FILESIZE -ge 20 ]
+	then
+		xFILENAME="$(echo $j |awk -F. '{print$1}')"
+		sort -t ',' -k 2,2 -u $j > ${xFILENAME}_sorted.txt
+		split -l 10 -d --additional-suffix=.txt ${xFILENAME}_sorted.txt ${xFILENAME}_sorted_
+		echo "File ${xFILENAME}.txt sorted dan sudah selesai di split."
+	elif [ $FILESIZE -lt 20 ]
+	then
+		xFILENAME="`echo $j |awk -F. '{print$1}'`"
+		sort -t ',' -k 2,2 -u $j > ${xFILENAME}_sorted.txt
+		echo "File ${xFILENAME}.txt sorted. Tidak perlu di split."
+	fi
 done
