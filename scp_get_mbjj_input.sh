22ae6c6cee3c0d36e2141b0e9a9898582636981f
Mickel Edward <o2tongpethuk@gmail.com>
2021-04-11 19:03:21 +0700

    script done

---
+++ scp_get_mbjj_input.sh
@@ -0,0 +1,19 @@
+tanggal=$1
+
+if [ -z $tanggal ]
+then
+	echo "Parameternya Harus Tanggal"
+	exit 1
+fi
+
+thn=${tanggal:0:4}
+bln=${tanggal:4:2}
+tgl=${tanggal:6:2}
+xtanggal="${thn}-${bln}-${tgl}"
+ytanggal="$(date -d "$tanggal +1 days" +"%Y-%m-%d")"
+
+listfile="$(ssh upcc_user@10.54.3.252 'find /data9/higt_bi/landing/pusdatin_mbjj_sptjm/upcc/input/*_upcc.txt -newermt $xtanggal ! -newermt $ytanggal')"
+for i in $listfile
+do
+        scp upcc_user@10.54.3.252:$i .
+done
---
+++ split_and_sort_mbjj_input.sh
@@ -0,0 +1,34 @@
+tanggal=$1
+
+if [ -z $tanggal ]
+then
+	echo "Parameternya Harus Tanggal"
+	exit 1
+fi
+
+thn=${tanggal:0:4}
+bln=${tanggal:4:2}
+tgl=${tanggal:6:2}
+xtanggal="${thn}-${bln}-${tgl}"
+ytanggal="$(date -d "$tanggal +1 days" +"%Y-%m-%d")"
+
+cd /data4/itops/upcc_script
+FILENAME="$(find *_upcc.txt -newermt $xtanggal ! -newermt $ytanggal)"
+
+for i in $FILENAME
+do
+	COUNTLINE=$(wc -l $i |awk '{print$1}')
+	xFILENAME="$(basename $i |awk -F. '{print$1}')"
+	
+	if [ $COUNTLINE -ge 699999 ]
+	then
+		sort -t ',' -k 2,2 -u $i >${xFILENAME}_sorted.tmp
+		split -l 500000 -d --additional-suffix=.txt ${xFILENAME}_sorted.tmp ${xFILENAME}_sorted_
+		rm ${xFILENAME}_sorted.tmp
+		echo "File $i sorted. Sudah selesai di split"
+	elif [ $COUNTLINE -lt 699999 ]
+	then
+		sort -t ',' -k 2,2 -u $i >${xFILENAME}_sorted.txt
+		echo "File $i sorted. Tidak perlu di split"
+	fi
+done
