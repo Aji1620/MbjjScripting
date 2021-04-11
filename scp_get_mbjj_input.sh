567706f81b22acf6fef9d2ac662a10ea4e846d1a
Mickel Edward <o2tongpethuk@gmail.com>
2021-04-11 19:33:05 +0700

    small fixes

--- scp_get_mbjj_input.sh
+++ scp_get_mbjj_input.sh
@@ -12,7 +12,7 @@ tgl=${tanggal:6:2}
 xtanggal="${thn}-${bln}-${tgl}"
 ytanggal="$(date -d "$tanggal +1 days" +"%Y-%m-%d")"
 
-listfile="$(ssh upcc_user@10.54.3.252 'find /data9/higt_bi/landing/pusdatin_mbjj_sptjm/upcc/input/*_upcc.txt -newermt $xtanggal ! -newermt $ytanggal')"
+listfile="$(ssh upcc_user@10.54.3.252 "find /data9/higt_bi/landing/pusdatin_mbjj_sptjm/upcc/input/*_upcc.txt -newermt $xtanggal ! -newermt $ytanggal")"
 for i in $listfile
 do
         scp upcc_user@10.54.3.252:$i .
