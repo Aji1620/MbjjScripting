474a669bd98ade80b224c7f564af0bafa04c2d12
Mickel Edward <o2tongpethuk@gmail.com>
2021-11-14 17:33:52 +0700

    Update validation_mbjj_input.sh

--- validation_mbjj_input.sh
+++ validation_mbjj_input.sh
@@ -1,43 +1,43 @@
-tanggal=$1
-type=$2
-
-if [[ -z $tanggal || -z $type ]]
+#!/bin/bash
+a="${$}"
+tanggal="${1}"
+type="${2^^}"
+. /data4/script_L1/linux_util_rifq.sh &>/dev/null
+if [[ -z "${tanggal}" || -z "${type}" ]]
 then
 	echo "Parameter Kosong"
 	echo "Silahkan input parameter tanggal dan tipe"
 	exit 1
 fi
 
-thn=${tanggal:0:4}
-bln=${tanggal:4:2}
-tgl=${tanggal:6:2}
-xtanggal="${thn}-${bln}-${tgl}"
-ytanggal="$(date -d "$tanggal +1 days" +"%Y-%m-%d")"
+xtanggal="${tanggal:0:4}-${tanggal:4:2}-${tanggal:6:2}"
+ytanggal="$(date -d "${xtanggal} +1 days" '+%Y-%m-%d')"
 
 cd /data4/itops/upcc_script
-FILENAME="$(find *_upcc.txt -newermt $xtanggal ! -newermt $ytanggal)"
+FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
 
-if [ $type == "MSISDN" ]
+if [ "${type}" == "MSISDN" ]
 then
-	echo 'FILENAME|MSISDN DUPLIKAT|TOTAL MSISDN' >msisdn_validation.tmp
-	for i in $FILENAME
+	echo 'FILENAME|MSISDN DUPLIKAT|TOTAL MSISDN' >msisdn_validation_${a}.tmp
+	for i in ${FILENAME}
 	do
-		echo -n "$(basename $i)|" >>msisdn_validation.tmp
-		echo -n "$(cat $i |awk -F, '{print$1}' |sort |uniq -cd |wc -l)|" >>msisdn_validation.tmp
-		echo "$(cat $i |wc -l)" >>msisdn_validation.tmp
+		echo -n "$(basename "${i}")|" >>msisdn_validation_${a}.tmp
+		echo -n "$(awk -F, '{print$1}' "${i}" |sort |uniq -cd |wc -l |numfmt --g)|" >>msisdn_validation_${a}.tmp
+		echo "$(wc -l <"${i}" |numfmt --g)" >>msisdn_validation_${a}.tmp
 	done
-	cat msisdn_validation.tmp |sed 's/[- ]*\([+|]\)/'$'\x01''\1/g' |column -ts $'\x01' |sed '/^[-+ ]*$/s/ /-/g' ;rm msisdn_validation.tmp
-elif [ $type == "PDID" ]
+	printTable '|' <msisdn_validation_${a}.tmp
+	rm -f msisdn_validation_${a}.tmp
+elif [ "${type}" == "PDID" ]
 then
-	echo 'FILENAME|PDID DUPLIKAT|TOTAL PDID' >pdid_validation.tmp
-	for i in $FILENAME
+	echo 'FILENAME|PDID DUPLIKAT|TOTAL PDID' >pdid_validation_${a}.tmp
+	for i in ${FILENAME}
 	do
-		echo -n "$(basename $i)|" >>pdid_validation.tmp
-		echo -n "$(cat $i |awk -F, '{print$2}' |sort |uniq -cd |wc -l)|" >>pdid_validation.tmp
-		echo "$(cat $i |wc -l)" >>pdid_validation.tmp
+		echo -n "$(basename "${i}")|" >>pdid_validation_${a}.tmp
+		echo -n "$(awk -F, '{print$2}' "${i}" |sort |uniq -cd |wc -l |numfmt --g)|" >>pdid_validation_${a}.tmp
+		echo "$(wc -l <"${i}" |numfmt --g)" >>pdid_validation_${a}.tmp
 	done
-	cat pdid_validation.tmp |sed 's/[- ]*\([+|]\)/'$'\x01''\1/g' |column -ts $'\x01' |sed '/^[-+ ]*$/s/ /-/g' ;rm pdid_validation.tmp
+	printTable '|' <pdid_validation_${a}.tmp
+	rm -f pdid_validation_${a}.tmp
 else
 	echo "Maaf Anda Salah Input Parameter, Param YG digunakan MSISDN OR PDID"
 fi
-
