5028bbe163dbb12799b14a0cba1db33f8e57e868
Mickel Edward <o2tongpethuk@gmail.com>
2021-11-26 23:41:32 +0700

    Update validation_mbjj_input.sh

--- validation_mbjj_input.sh
+++ validation_mbjj_input.sh
@@ -3,11 +3,9 @@ a="${$}"
 tanggal="${1}"
 type="${2^^}"
 . /data4/script_L1/linux_util_rifq.sh &>/dev/null
-if [[ -z "${tanggal}" || -z "${type}" ]]
-then
-	echo "Parameter Kosong"
-	echo "Silahkan input parameter tanggal dan tipe"
-	exit 1
+if [ "${#}" -lt '2' ]; then
+    echo -e "Cannot continue. The given parameter is not complete"
+    exit 1
 fi
 
 xtanggal="${tanggal:0:4}-${tanggal:4:2}-${tanggal:6:2}"
@@ -16,28 +14,33 @@ ytanggal="$(date -d "${xtanggal} +1 days" '+%Y-%m-%d')"
 cd /data4/itops/upcc_script
 FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
 
-if [ "${type}" == "MSISDN" ]
-then
-	echo 'FILENAME|MSISDN DUPLIKAT|TOTAL MSISDN' >msisdn_validation_${a}.tmp
-	for i in ${FILENAME}
-	do
-		echo -n "$(basename "${i}")|" >>msisdn_validation_${a}.tmp
-		echo -n "$(awk -F, '{print$1}' "${i}" |sort |uniq -cd |wc -l |numfmt --g)|" >>msisdn_validation_${a}.tmp
-		echo "$(wc -l <"${i}" |numfmt --g)" >>msisdn_validation_${a}.tmp
-	done
-	printTable '|' <msisdn_validation_${a}.tmp
-	rm -f msisdn_validation_${a}.tmp
-elif [ "${type}" == "PDID" ]
-then
-	echo 'FILENAME|PDID DUPLIKAT|TOTAL PDID' >pdid_validation_${a}.tmp
-	for i in ${FILENAME}
-	do
-		echo -n "$(basename "${i}")|" >>pdid_validation_${a}.tmp
-		echo -n "$(awk -F, '{print$2}' "${i}" |sort |uniq -cd |wc -l |numfmt --g)|" >>pdid_validation_${a}.tmp
-		echo "$(wc -l <"${i}" |numfmt --g)" >>pdid_validation_${a}.tmp
-	done
-	printTable '|' <pdid_validation_${a}.tmp
-	rm -f pdid_validation_${a}.tmp
-else
-	echo "Maaf Anda Salah Input Parameter, Param YG digunakan MSISDN OR PDID"
-fi
+case "${type}" in
+"MSISDN")
+    {
+        echo 'FILENAME|MSISDN DUPLIKAT|TOTAL MSISDN'
+        for i in ${FILENAME}; do
+            echo -n "$(basename ${i})|"
+            echo -n "$(awk -F, '{print $1}' ${i} | sort | uniq -cd | wc -l | numfmt --g)|"
+            echo "$(wc -l <${i} | numfmt --g)"
+        done
+    } | printTable '|'
+    ;;
+"PDID")
+    {
+        echo 'FILENAME|PDID DUPLIKAT|TOTAL PDID'
+        for i in ${FILENAME}; do
+            echo -n "$(basename ${i})|"
+            echo -n "$(awk -F, '{print$2}' ${i} | sort | uniq -cd | wc -l | numfmt --g)|"
+            echo "$(wc -l <${i} | numfmt --g)"
+        done
+    } | printTable '|'
+    ;;
+*)
+    echo -e "Invalid Type"
+    echo -e "Type entered: ${type}"
+    echo -e "Valid Type:"
+    echo -e "1. MSISDN"
+    echo -e "2. PDID"
+    exit 1
+    ;;
+esac
