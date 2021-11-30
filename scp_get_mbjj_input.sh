b1227adc405c132d7ce3d7acc1acc2a9da3abd8f
Mickel Edward <o2tongpethuk@gmail.com>
2021-11-30 18:00:16 +0700

    Update scp_get_mbjj_input.sh

--- scp_get_mbjj_input.sh
+++ scp_get_mbjj_input.sh
@@ -1,19 +1,16 @@
-tanggal=$1
-
-if [ -z $tanggal ]
-then
-	echo "Parameternya Harus Tanggal"
-	exit 1
+#!/bin/bash
+tanggal="${1}"
+if [ -z "${tanggal}" ]; then
+    echo -e 'Cannot continue. The given parameter is not complete'
+    exit 1
 fi
-
-thn=${tanggal:0:4}
-bln=${tanggal:4:2}
-tgl=${tanggal:6:2}
+thn="${tanggal:0:4}"
+bln="${tanggal:4:2}"
+tgl="${tanggal:6:2}"
 xtanggal="${thn}-${bln}-${tgl}"
-ytanggal="$(date -d "$tanggal +1 days" +"%Y-%m-%d")"
+ytanggal="$(date -d "${tanggal} +1 days" '+%Y-%m-%d')"
 
-listfile="$(ssh upcc_user@10.54.3.252 "find /data9/higt_bi/landing/pusdatin_mbjj_sptjm/upcc/input/*_upcc.txt -newermt $xtanggal ! -newermt $ytanggal")"
-for i in $listfile
-do
-        scp upcc_user@10.54.3.252:$i .
+listfile="$(ssh upcc_user@10.54.3.252 "find /data9/higt_bi/landing/pusdatin_mbjj_sptjm/upcc/input/*_upcc.txt -newermt '${xtanggal}' ! -newermt '${ytanggal}'")"
+for i in ${listfile}; do
+    scp "upcc_user@10.54.3.252:${i}" .
 done
