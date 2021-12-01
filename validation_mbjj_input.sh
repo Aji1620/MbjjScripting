b666ed04a27dacf3be139bf3bfac86cf6ab965c9
Mickel Edward <o2tongpethuk@gmail.com>
2021-12-02 00:14:37 +0700

    Update validation_mbjj_input.sh

--- validation_mbjj_input.sh
+++ validation_mbjj_input.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
-a="${$}"
-tanggal="${1}"
-type="${2^^}"
+#!/usr/bin/env bash
 . /data4/script_L1/linux_util_rifq.sh &>/dev/null
-if [ "${#}" -lt '2' ]; then
-    echo -e "Cannot continue. The given parameter is not complete"
-    exit 1
-fi
-
+a="${$}"
+type="${1^^}"
+tanggal="${2}"
 xtanggal="${tanggal:0:4}-${tanggal:4:2}-${tanggal:6:2}"
 ytanggal="$(date -d "${xtanggal} +1 days" '+%Y-%m-%d')"
 
 cd /data4/itops/upcc_script
-FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
+if [ -t 0 ]; then
+    if [ "${#}" -lt '2' ]; then
+        echo -e "Cannot continue. The given parameter is not complete"
+        exit 1
+    else
+        FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
+    fi
+else
+    FILENAME="$(cat)"
+fi
 
 case "${type}" in
 "MSISDN")
