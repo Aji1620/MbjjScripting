9ab8b18712db080055bb5829ec246c82eb816d55
Mickel Edward <o2tongpethuk@gmail.com>
2021-12-02 16:13:54 +0700

    Update split_and_sort_mbjj_input.sh

--- split_and_sort_mbjj_input.sh
+++ split_and_sort_mbjj_input.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 datex="${1}"
-xtanggal="${datex:0:4}-${datex:4:2}-${datex:6:2}"
-ytanggal="$(date -d "${tanggal} +1 days" '+%Y-%m-%d')"
 
 cd /data4/itops/upcc_script
-if [ -t 0 ]; then
-    if [ -z "${datex}" ]; then
-        echo -e "Cannot continue. The given parameter is not complete"
-        exit 1
-    else
-        FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
-    fi
-else
+if [ ! -t 0 ]; then
     FILENAME="$(cat)"
+else
+    if [ -z "${tanggal}" ]; then
+        echo -e 'Cannot continue. The given parameter is not complete'
+        exit 1
+    elif [ -n "${tanggal}" ]; then
+        truedate="$(date -d "${tanggal}" '+%Y%m%d' 2>/dev/null)"
+        if [ "${truedate}" != "${tanggal}" ]; then
+            echo -e 'Format tanggal tidak sesuai, mohon dicek kembali'
+            exit 1
+        else
+            xtanggal="${tanggal:0:4}-${tanggal:4:2}-${tanggal:6:2}"
+            ytanggal="$(date -d "${xtanggal} +1 days" '+%Y-%m-%d' 2>/dev/null)"
+            FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
+        fi
+    fi
 fi
 
 for i in ${FILENAME}; do
