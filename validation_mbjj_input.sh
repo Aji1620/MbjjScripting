d2c8d83a52fe03f114de0345e9db4a58b6d7f4d7
Mickel Edward <o2tongpethuk@gmail.com>
2021-12-14 10:33:04 +0700

    Update validation_mbjj_input.sh

--- validation_mbjj_input.sh
+++ validation_mbjj_input.sh
@@ -18,7 +18,7 @@ else
         else
             xtanggal="${tanggal:0:4}-${tanggal:4:2}-${tanggal:6:2}"
             ytanggal="$(date -d "${xtanggal} +1 days" '+%Y-%m-%d' 2>/dev/null)"
-            FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 {} +)"
+            FILENAME="$(find -maxdepth '1' -type 'f' -name '*_upcc.txt' -newermt "${xtanggal}" ! -newermt "${ytanggal}" -exec ls -1 '{}' +)"
         fi
     fi
 fi
@@ -27,9 +27,9 @@ fi
     echo 'FILENAME|MSISDN DUPLIKAT|UNIQ MSISDN|PDID DUPLIKAT|UNIQ PDID|TOTAL RECORD'
     for i in ${FILENAME}; do
         echo -n "$(basename ${i})|"
-        echo -n "$(awk -F ',' '{print $1}' ${i} | sort | uniq -cd | wc -l | numfmt --g)|"
+        echo -n "$(awk -F ',' 'x[$1]++ == 1 {print $1}' ${i} | wc -l | numfmt --g)|"
         echo -n "$(awk -F ',' '!x[$1]++ {print $1}' ${i} | wc -l | numfmt --g)|"
-        echo -n "$(awk -F ',' '{print $2}' ${i} | sort | uniq -cd | wc -l | numfmt --g)|"
+        echo -n "$(awk -F ',' 'x[$2]++ == 1 {print $2}' ${i} | wc -l | numfmt --g)|"
         echo -n "$(awk -F ',' '!x[$2]++ {print $2}' ${i} | wc -l | numfmt --g)|"
         echo "$(wc -l <${i} | numfmt --g)"
     done
