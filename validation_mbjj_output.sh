04cc0a7f159a95dd019803a2c772abb0af2b02e9
Mickel Edward <o2tongpethuk@gmail.com>
2021-12-15 11:52:27 +0700

    Update validation_mbjj_output.sh

--- validation_mbjj_output.sh
+++ validation_mbjj_output.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . /data4/script_L1/linux_util_rifq.sh &>/dev/null
 a="${$}"
-FILENAME="${1}"
+FILENAME="${@}"
 
 cd /data4/itops/upcc_script
 if [ ! -t 0 ]; then
@@ -15,11 +15,11 @@ fi
 
 for i in ${FILENAME}; do
     cat <<EOF
---------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
 FILENAME   : $(basename ${i})
 START TIME : $(awk -F '|' 'NF >= "5" {print $1}' ${i} | sort -t ',' -k 4 | awk '{print $2}' | head -1)
 END TIME   : $(awk -F '|' 'NF >= "5" {print $1}' ${i} | sort -t ',' -k 4 | awk '{print $2}' | tail -1)
-DETAIL     : $(awk -F '|' 'NF >= "5" {col[$2"|"$3"|"$4"|"$5]++} END {for (i in col) print i" = "col[i]}' ${i})
---------------------------------------------------
+RESULT     : $(awk -F '|' 'NF >= "5" {col[$2"|"$3"|"$4"|"$5]++} END {for (i in col) print i" = "col[i]}' ${i})
+--------------------------------------------------------------------------------------------------------------
 EOF
 done
