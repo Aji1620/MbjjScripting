9d70f5f6ea994d2810fc247dbae55a8cab3edf9a
Mickel Edward <o2tongpethuk@gmail.com>
2021-12-15 10:34:07 +0700

    Create validation_mbjj_output.sh

---
+++ validation_mbjj_output.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+. /data4/script_L1/linux_util_rifq.sh &>/dev/null
+a="${$}"
+FILENAME="${1}"
+
+cd /data4/itops/upcc_script
+if [ ! -t 0 ]; then
+    FILENAME="$(cat)"
+else
+    if [ -z "${FILENAME}" ]; then
+        echo -e 'Cannot continue. The given parameter is not complete'
+        exit 1
+    fi
+fi
+
+for i in ${FILENAME}; do
+    cat <<EOF
+--------------------------------------------------
+FILENAME   : $(basename ${i})
+START TIME : $(awk -F '|' 'NF >= "5" {print $1}' ${i} | sort -t ',' -k 4 | awk '{print $2}' | head -1)
+END TIME   : $(awk -F '|' 'NF >= "5" {print $1}' ${i} | sort -t ',' -k 4 | awk '{print $2}' | tail -1)
+DETAIL     : $(awk -F '|' 'NF >= "5" {col[$2"|"$3"|"$4"|"$5]++} END {for (i in col) print i" = "col[i]}' ${i})
+--------------------------------------------------
+EOF
+done
